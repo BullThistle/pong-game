833df6c90e1820393a0f1340c7c0df683e6757ef
Rafael Furry <rfurry@gmail.com>
2017-08-18 15:11:19 -0700

    creates Vec, Rect and Ball classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,40 @@
+class Vec
+{
+    constructor(x = 0, y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Rect
+{
+    constructor(w,h)
+    {
+        this.pos = new Vec;
+        this.size = new Vec(w,h);
+    }
+}
+
+class Ball extends Rect
+{
+    constructor()
+    {
+        super(10, 10)
+        this.vel = new Vec;
+    }
+}
+
+const ball = new Ball;
+console.log(ball);
+
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
 context.fillStyle = '#000';
-
 context.fillRect( 0, 0, canvas.width, canvas.height);
 
+context.fillStyle = '#fff';
+context.fillRect(0, 0, 10, 10);
 
 
-
-pong.start();
