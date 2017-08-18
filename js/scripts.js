17e9e06abd90c7b094f7c67e5f25119f30672c21
Rafael Furry <rfurry@gmail.com>
2017-08-18 15:02:55 -0700

    fills the rectangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
+const canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+
+context.fillRect( 0, 0, canvas.width, canvas.height);
+
+
+
+
 pong.start();
